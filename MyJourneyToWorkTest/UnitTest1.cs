075efd95d330e7090476eddac8d2f61ebdd62928
Andrei Ciobirca <aciobirca97@gmail.com>
2023-11-23 21:21:47 +0000

    Added new tests for coverage

--- MyJourneyToWorkTest/UnitTest1.cs
+++ MyJourneyToWorkTest/UnitTest1.cs
@@ -1,49 +1,44 @@
 using Calculator;
+using Microsoft.AspNetCore.Mvc;
 using MyJourneyToWork.Pages;
+
 namespace MyJourneyToWorkTest
 {
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.RazorPages;
-    using MyJourneyToWork.Pages;
-    using NUnit.Framework;
-
-
-    [TestFixture]
-    public class CalculatorTests
+    public class Tests
     {
+
         [Test]
         public void ConvertDistance_ConvertsKilometersToMiles()
         {
             // Arrange
             var calculator = new Calculator.Calculator
             {
-                distance = 10,
+                distance = 160.9344, // 100 kilometers
                 milesOrKms = DistanceMeasurement.kms
             };
 
             // Act
-            var convertedDistance = calculator.convertDistance();
+            var result = calculator.convertDistance();
 
             // Assert
-            Assert.AreEqual(6.21371, convertedDistance, 0.00001); // Allow for small precision error
+            Assert.AreEqual(100, result);
         }
 
         [Test]
-        public void ConvertDistance_DoesNotConvertMiles()
+        public void ConvertDistance_LeavesMilesUnchanged()
         {
             // Arrange
             var calculator = new Calculator.Calculator
             {
-                distance = 10,
+                distance = 100, // 100 miles
                 milesOrKms = DistanceMeasurement.miles
             };
 
             // Act
-            var convertedDistance = calculator.convertDistance();
+            var result = calculator.convertDistance();
 
             // Assert
-            Assert.AreEqual(10, convertedDistance);
+            Assert.AreEqual(100, result);
         }
 
         [Test]
@@ -52,257 +47,37 @@ namespace MyJourneyToWorkTest
             // Arrange
             var calculator = new Calculator.Calculator
             {
-                distance = 10,
+                distance = 50, // 50 miles
                 milesOrKms = DistanceMeasurement.miles,
                 numDays = 3,
                 transportMode = TransportModes.petrol
             };
 
             // Act
-            var sustainabilityWeighting = calculator.sustainabilityWeighting;
+            var result = calculator.sustainabilityWeighting;
 
             // Assert
-            Assert.AreEqual(480, sustainabilityWeighting);
+            Assert.AreEqual(8 * 50 * (3 * 2), result);
         }
 
-
         [Test]
-        public void SustainabilityWeighting_CalculatesCorrectlyForElectricBike()
+        public void SustainabilityWeighting_CalculatesCorrectlyForCycling()
         {
             // Arrange
             var calculator = new Calculator.Calculator
             {
-                distance = 8,
-                milesOrKms = DistanceMeasurement.kms,
-                numDays = 1,
-                transportMode = TransportModes.electricbike
-            };
-
-            // Act
-            var sustainabilityWeighting = calculator.sustainabilityWeighting;
-
-            // Assert
-            Assert.AreEqual(19.883878151594686, sustainabilityWeighting, 0.000001);
-        }
-
-        [Test]
-        public void ConvertDistance_ReturnsSameDistanceForMiles()
-        {
-            // Arrange
-            var calculator = new Calculator.Calculator
-            {
-                distance = 15,
-                milesOrKms = DistanceMeasurement.miles
-            };
-
-            // Act
-            var convertedDistance = calculator.convertDistance();
-
-            // Assert
-            Assert.AreEqual(15, convertedDistance);
-        }
-
-        [Test]
-        public void ConvertDistance_ReturnsZeroForZeroDistance()
-        {
-            // Arrange
-            var calculator = new Calculator.Calculator
-            {
-                distance = 0,
-                milesOrKms = DistanceMeasurement.kms
-            };
-
-            // Act
-            var convertedDistance = calculator.convertDistance();
-
-            // Assert
-            Assert.AreEqual(0, convertedDistance);
-        }
-
-        [Test]
-        public void SustainabilityWeighting_ReturnsZeroForZeroDays()
-        {
-            // Arrange
-            var calculator = new Calculator.Calculator
-            {
-                distance = 20,
+                distance = 10, // 10 miles
                 milesOrKms = DistanceMeasurement.miles,
-                numDays = 0,
-                transportMode = TransportModes.bus
+                numDays = 5,
+                transportMode = TransportModes.cycling
             };
 
             // Act
-            var sustainabilityWeighting = calculator.sustainabilityWeighting;
+            var result = calculator.sustainabilityWeighting;
 
             // Assert
-            Assert.AreEqual(0, sustainabilityWeighting);
+            Assert.AreEqual(0.005 * 10 * (5 * 2), result);
         }
 
-        [Test]
-        public void SustainabilityWeighting_ReturnsZeroForUnknownTransportMode()
-        {
-            // Arrange
-            var calculator = new Calculator.Calculator
-            {
-                distance = 25,
-                milesOrKms = DistanceMeasurement.kms,
-                numDays = 3,
-                transportMode = (TransportModes)999 // Unknown transport mode
-            };
-
-            // Act
-            var sustainabilityWeighting = calculator.sustainabilityWeighting;
-
-            // Assert
-            Assert.AreEqual(0, sustainabilityWeighting);
-        }
-
-        [Test]
-        public void SustainabilityWeighting_ReturnsZeroForZeroDistance()
-        {
-            // Arrange
-            var calculator = new Calculator.Calculator
-            {
-                distance = 0,
-                milesOrKms = DistanceMeasurement.kms,
-                numDays = 3,
-                transportMode = TransportModes.bus
-            };
-
-            // Act
-            var sustainabilityWeighting = calculator.sustainabilityWeighting;
-
-            // Assert
-            Assert.AreEqual(0, sustainabilityWeighting);
-        }
-
-        [Test]
-        public void ConvertDistance_ConvertsKilometersToMiles_ForCalculatorInstance()
-        {
-            // Arrange
-            var calculator = new Calculator.Calculator
-            {
-                distance = 10,
-                milesOrKms = DistanceMeasurement.kms
-            };
-
-            // Act
-            var convertedDistance = calculator.convertDistance();
-
-            // Assert
-            Assert.AreEqual(6.21371, convertedDistance, 0.00001); // Allow for small precision error
-        }
-
-        [Test]
-        public void ConvertDistance_DoesNotConvertMiles_ForCalculatorInstance()
-        {
-            // Arrange
-            var calculator = new Calculator.Calculator
-            {
-                distance = 10,
-                milesOrKms = DistanceMeasurement.miles
-            };
-
-            // Act
-            var convertedDistance = calculator.convertDistance();
-
-            // Assert
-            Assert.AreEqual(10, convertedDistance);
-        }
-
-        [Test]
-        public void SustainabilityWeighting_CalculatesCorrectlyForPetrol_ForCalculatorInstance()
-        {
-            // Arrange
-            var calculator = new Calculator.Calculator
-            {
-                distance = 10,
-                milesOrKms = DistanceMeasurement.miles,
-                numDays = 3,
-                transportMode = TransportModes.petrol
-            };
-
-            // Act
-            var sustainabilityWeighting = calculator.sustainabilityWeighting;
-
-            // Assert
-            Assert.AreEqual(480, sustainabilityWeighting);
-        }
-
-        //Testing for Privacy.cshtml.cs
-
-        [TestFixture]
-        public class CalculatorModelTests
-        {
-            [Test]
-            public void OnGet_Should_NotThrowException()
-            {
-                // Arrange
-                var calculatorModel = new CalculatorModel();
-
-                // Act & Assert
-                Assert.DoesNotThrow(() => calculatorModel.OnGet());
-            }
-
-            [Test]
-            public void CalculatorProperty_Should_HaveBindPropertyAttribute()
-            {
-                // Arrange
-                var calculatorProperty = typeof(CalculatorModel).GetProperty("calculator");
-
-                // Act & Assert
-                Assert.IsNotNull(calculatorProperty);
-                Assert.IsTrue(calculatorProperty.GetCustomAttributes(typeof(BindPropertyAttribute), true).Length > 0);
-            }
-
-            [Test]
-            public void ConvertDistance_ConvertsKmsToMiles()
-            {
-                // Arrange
-                var calculator = new Calculator.Calculator { distance = 10, milesOrKms = DistanceMeasurement.kms };
-
-                // Act
-                var result = calculator.convertDistance();
-
-                // Assert
-                Assert.AreEqual(6.21371, result, 0.00001); // Check with a delta for floating-point precision
-            }
-
-            [Test]
-            public void ConvertDistance_DoesNotConvertMiles()
-            {
-                // Arrange
-                var calculator = new Calculator.Calculator { distance = 10, milesOrKms = DistanceMeasurement.miles };
-
-                // Act
-                var result = calculator.convertDistance();
-
-                // Assert
-                Assert.AreEqual(10, result);
-            }
-
-            [Test]
-            public void SustainabilityWeighting_CalculatesCorrectlyForPetrol()
-            {
-                // Arrange
-                var calculator = new Calculator.Calculator
-                {
-                    distance = 10,
-                    milesOrKms = DistanceMeasurement.miles,
-                    numDays = 3,
-                    transportMode = TransportModes.petrol
-                };
-
-                // Act
-                var result = calculator.sustainabilityWeighting;
-
-                // Assert
-                Assert.AreEqual(480, result);
-            }
-
-        }
     }
 }
-
-
-
