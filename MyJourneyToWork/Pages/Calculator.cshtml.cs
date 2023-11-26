16a395078a7b93a5bf71fef7166c32a9470aa437
Andrei Ciobirca <aciobirca97@gmail.com>
2023-11-26 15:06:43 +0000

    reverting to working code

--- MyJourneyToWork/Pages/Calculator.cshtml.cs
+++ MyJourneyToWork/Pages/Calculator.cshtml.cs
@@ -6,14 +6,11 @@ namespace MyJourneyToWork.Pages
 {
     public class CalculatorModel : PageModel
     {
-        [BindProperty]
+        [BindProperty]      
         public Calculator.Calculator calculator { get; set; }
 
         public void OnGet()
         {
-            // This method is intentionally left empty as there is nothing to do on a GET request.
-            // The functionality might be extended in the future.
-            // For now, it serves as a placeholder.
         }
 
         public void OnPost()
@@ -27,38 +24,16 @@ namespace MyJourneyToWork.Pages
 
         public void SaveCalculationHistory()
         {
-            try
-            {
-                var history = HttpContext.Session.GetString("CalculationHistory");
-                var historyList = history != null ? JsonConvert.DeserializeObject<List<double>>(history) : new List<double>();
-                historyList.Add(calculator.sustainabilityWeighting);
-                HttpContext.Session.SetString("CalculationHistory", JsonConvert.SerializeObject(historyList));
-            }
-            catch (Exception ex)
-            {
-                // Handle exceptions, log, or rethrow based on your application's error handling strategy.
-                // Example: Log.Error($"Failed to save calculation history. Error: {ex.Message}");
-                throw;
-            }
+            var history = HttpContext.Session.GetString("CalculationHistory");
+            var historyList = history != null ? JsonConvert.DeserializeObject<List<double>>(history) : new List<double>();
+            historyList.Add(calculator.sustainabilityWeighting);
+            HttpContext.Session.SetString("CalculationHistory", JsonConvert.SerializeObject(historyList));
         }
-
-
 
         public List<double> GetCalculationHistory()
         {
-            try
-
-            {
-                var history = HttpContext.Session.GetString("CalculationHistory");
-                return history != null ? JsonConvert.DeserializeObject<List<double>>(history) : new List<double>();
-            }
-            catch (Exception ex)
-            {
-                // Handle exceptions, log, or rethrow based on your application's error handling strategy.
-                // Example: Log.Error($"Failed to retrieve calculation history. Error: {ex.Message}");
-                throw;
-            }
-
+            var history = HttpContext.Session.GetString("CalculationHistory");
+            return history != null ? JsonConvert.DeserializeObject<List<double>>(history) : new List<double>();
         }
     }
 }
